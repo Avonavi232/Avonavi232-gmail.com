275648bb6009c355a85017f728bc0bdadb3a6fed
Иванов Александр <a.ivanov@gph.cian.ru>
2020-03-27 22:53:12 +0300

    Updates

---
+++ precache-manifest.0efa776df94c7d19dc1749bb9715e70d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec7a34799d567d28b5095cd3cf51793",
+    "url": "/chart/index.html"
+  },
+  {
+    "revision": "535b3512fa69f70839b3",
+    "url": "/chart/static/css/main.71c8d471.chunk.css"
+  },
+  {
+    "revision": "20415de50517b9895e09",
+    "url": "/chart/static/js/2.183c8000.chunk.js"
+  },
+  {
+    "revision": "8b6ad969ce7a1d625c347a24047a0e8d",
+    "url": "/chart/static/js/2.183c8000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535b3512fa69f70839b3",
+    "url": "/chart/static/js/main.4550bbdc.chunk.js"
+  },
+  {
+    "revision": "1a107852a6787a96f332",
+    "url": "/chart/static/js/runtime-main.d80aa42a.js"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/chart/static/media/Roboto-Bold.08cb8f79.woff"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/chart/static/media/Roboto-Regular.94dac78e.woff"
+  }
+]);
